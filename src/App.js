26eac15814b0a7527017c2cc3d36c1d3111bc742
Kommaraju Hara Narayana Karthik <karthikkommaraju1563@gmail.com>
2025-01-22 20:49:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import react router dom
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import Product from './pages/ProductDetails';
+import PersistentDrawerLeft from './components/Sidebar';
+// import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+  return <div className='overflow-hidden'>
+    <Router>
+      {/* <Header/> */}
+      <PersistentDrawerLeft/>
+      <Routes>
+        <Route path = '/' element={<Home/>}/>
+        <Route path = '/product/:id' element={<Product/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  </div>;
+};
+
+export default App;
