fbba74ccaf44b776bdcd55e9a8d40544e57eeab4
Kommaraju Hara Narayana Karthik <karthikkommaraju1563@gmail.com>
2025-01-29 21:25:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Home from './pages/Home';
 import Product from './pages/ProductDetails';
 import PersistentDrawerLeft from './components/Sidebar';
 // import Header from './components/Header';
-import Footer from './components/Footer';
-const App = () => {
+// import Footer from './components/Footer';
+const App = () => 
+{
   return <div className='overflow-hidden'>
     <Router>
       {/* <Header/> */}
@@ -16,7 +17,7 @@ const App = () => {
         <Route path = '/' element={<Home/>}/>
         <Route path = '/product/:id' element={<Product/>}/>
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </Router>
   </div>;
 };
