7cb24e50bd5aa43e277b22479359b95c8696192b
Kommaraju Hara Narayana Karthik <karthikkommaraju1563@gmail.com>
2025-01-29 21:24:59 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+export default function FooterNavigation() {
+  const [value, setValue] = React.useState(0);
 
-export default Footer;
+  return (
+    <Box
+      sx={{ 
+        width: '100%', 
+        position: 'fixed', 
+        bottom: 2, 
+        backgroundColor: 'background.paper', 
+        boxShadow: 3 
+      }}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
+        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} />
+        <BottomNavigationAction label="Mail" icon={<EmailIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
